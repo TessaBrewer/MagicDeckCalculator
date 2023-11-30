939d4d200e6315d42ba649401e66c89a7bde0578
Tessa Brewer <tessa.brewer@bearingpoint.com>
2023-11-30 00:42:02 +0000

    Init

---
+++ MagicOddsCalc/Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicOddsCalc
+{
+    internal class Node
+    {
+        private Dictionary<char, int> Deck;
+        private string CardsDrawn;
+        private double Odds;
+        private List<Node> Children = new List<Node>();
+
+        public Node(Dictionary<char, int> Deck)
+        {
+            Odds = 1.0;
+
+            CardsDrawn = "";
+
+            this.Deck = Deck;
+        }
+
+        public Node(Dictionary<char, int> PrevDeck, string PreviousDraws, char TopCard, double Odds)
+        {
+            this.Odds = Odds;
+
+            CardsDrawn = PreviousDraws + TopCard;
+
+            int CategoryCount = PrevDeck[TopCard];
+            int NewCount = CategoryCount - 1;
+
+            Deck = PrevDeck.ToDictionary(e => e.Key, e => e.Value);
+            Deck[TopCard] = NewCount;
+        }
+
+        public void Spawn(int DepthRemaining)
+        {
+            int CardsInDeck = 0;
+            foreach (KeyValuePair<char, int> kv in Deck) { CardsInDeck += kv.Value; }
+
+            foreach (KeyValuePair <char, int> kv in Deck)
+            {
+                if (kv.Value <= 0)
+                    continue;
+
+                double NewOdds = (double)kv.Value / CardsInDeck;
+                NewOdds = Odds * NewOdds;
+                Node Child = new Node(Deck, CardsDrawn, kv.Key, NewOdds);
+                Children.Add(Child);
+            }
+
+            if (DepthRemaining > 0) { foreach (Node Child in Children) { Child.Spawn(DepthRemaining - 1); } }
+        }
+
+        public void PrintPretty(string indent, bool last)
+        {
+            Console.Write(indent);
+            if (last)
+            {
+                Console.Write("\\-");
+                indent += "  ";
+            } 
+            else
+            {
+                Console.Write("|-");
+                indent += "| ";
+            }
+            Console.WriteLine(CardsDrawn + " : " + Odds);
+
+            for (int i = 0; i < Children.Count; i++)
+            {
+                Children[i].PrintPretty(indent, i == Children.Count - 1);
+            }
+        }
+    }
+}
---
+++ MagicOddsCalc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace MagicOddsCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check that we have an even number of args
+            if(args.Length % 2 == 0)
+            {
+                throw new Exception("Must have an odd number of args");
+            }
+
+            int TreeDepth = int.Parse(args[0]);
+
+            //Build the symbol : count map
+            Dictionary<char, int> SymbolCounts = new Dictionary<char, int>();
+            for (int i = 1; i < args.Length; i += 2)
+            {
+                char Symbol = char.Parse(args[i]);
+                int Count = int.Parse(args[i + 1]);
+                SymbolCounts.Add(Symbol, Count);
+            }
+
+            //Build root node
+            Node Tree = new Node(SymbolCounts);
+            Tree.Spawn(TreeDepth);
+
+            //Print!
+            Tree.PrintPretty("", false);
+        }
+    }
+}
